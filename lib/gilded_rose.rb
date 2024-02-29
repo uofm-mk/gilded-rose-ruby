9715fbd2e8d4b83af3859164a388812315ba7522
Michael Kingma <mikingma@memphis.edu>
2024-02-28 20:44:01 -0600

    Added sulfuras_tick and backstage_tick, all tests pass

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -14,6 +14,10 @@ class GildedRose
       return normal_tick
     when 'Aged Brie'
       return brie_tick
+    when 'Sulfuras, Hand of Ragnaros'
+      return sulfuras_tick
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      return backstage_tick
     end
     if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
       if @quality > 0
@@ -58,6 +62,18 @@ class GildedRose
         end
       end
     end
+  end
+  def backstage_tick
+    @days_remaining -= 1
+    return    if @quality >=50
+    return @quality = 0 if @days_remaining < 0
+
+    @quality += 1
+    @quality += 1 if @days_remaining < 10
+    @quality += 1 if @days_remaining < 5
+  end
+  def sulfuras_tick
+
   end
   def brie_tick
     @days_remaining -= 1
