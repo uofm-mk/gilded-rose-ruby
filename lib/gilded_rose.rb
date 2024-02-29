9c28505e9882071bae2a4326ac1cb86a780d4905
Michael Kingma <mikingma@memphis.edu>
2024-02-28 22:09:12 -0600

    Implement conjured, tests not working

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,27 +1,12 @@
-class GildedRose
-
-  def self.for(name, quality, days_remaining)
-    @item = klass_for(name).new(quality, days_remaining)
-  end
-
-  def self.klass_for(name)
-    case name
-    when 'normal'
-      Normal
-    when 'Aged Brie'
-      Brie
-    when 'Sulfuras, Hand of Ragnaros'
-      Sulfuras
-    when 'Backstage passes to a TAFKAL80ETC concert'
-      Backstage
-    end
-  end
+module GildedRose
 
   class Item
     attr_reader :quality, :days_remaining
 
     def initialize(quality, days_remaining)
       @quality, @days_remaining = quality, days_remaining
+    end
+    def tick
     end
   end
 
@@ -45,11 +30,6 @@ class GildedRose
     end
   end
 
-  class Sulfuras < Item
-    def tick
-    end
-  end
-
   class Backstage < Item
     def tick
       @days_remaining -= 1
@@ -60,5 +40,26 @@ class GildedRose
     @quality += 1 if @days_remaining < 10
     @quality += 1 if @days_remaining < 5
     end
+  end
+
+  class Conjured < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+
+      @quality -= 2
+      @quality -= 2 if @days_remaining <= 0
+    end
+  end
+
+  DEFAULT_CLASS = Item
+  SPECIALIZED_CLASSES = {
+    'normal'                => Normal,
+    'Aged Brie'             => Brie,
+    'Backstage passes to a TAFKAL80ETC concert'     => Backstage,
+    'Conjured Mana Cake'        => Conjured }
+
+  def self.new(name, quality, days_remaining)
+    (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).new(quality, days_remaining)
   end
 end
