13fd31de21424edcd9532bd7c92fd9cd64d6c183
Michael Kingma <mikingma@memphis.edu>
2024-02-28 20:50:28 -0600

    delete original conditional

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -19,50 +19,8 @@ class GildedRose
     when 'Backstage passes to a TAFKAL80ETC concert'
       return backstage_tick
     end
-    if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
-      if @quality > 0
-        if @name != "Sulfuras, Hand of Ragnaros"
-          @quality = @quality - 1
-        end
-      end
-    else
-      if @quality < 50
-        @quality = @quality + 1
-        if @name == "Backstage passes to a TAFKAL80ETC concert"
-          if @days_remaining < 11
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-          if @days_remaining < 6
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-        end
-      end
-    end
-    if @name != "Sulfuras, Hand of Ragnaros"
-      @days_remaining = @days_remaining - 1
-    end
-    if @days_remaining < 0
-      if @name != "Aged Brie"
-        if @name != "Backstage passes to a TAFKAL80ETC concert"
-          if @quality > 0
-            if @name != "Sulfuras, Hand of Ragnaros"
-              @quality = @quality - 1
-            end
-          end
-        else
-          @quality = @quality - @quality
-        end
-      else
-        if @quality < 50
-          @quality = @quality + 1
-        end
-      end
-    end
   end
+
   def backstage_tick
     @days_remaining -= 1
     return    if @quality >=50
@@ -72,9 +30,10 @@ class GildedRose
     @quality += 1 if @days_remaining < 10
     @quality += 1 if @days_remaining < 5
   end
-  def sulfuras_tick
 
+  def sulfuras_tick
   end
+
   def brie_tick
     @days_remaining -= 1
     return if @quality >= 50
@@ -82,6 +41,7 @@ class GildedRose
     @quality += 1
     @quality += 1 if @days_remaining <= 0 && @quality < 50
   end
+
   def normal_tick
     @days_remaining -= 1
     return if @quality == 0
