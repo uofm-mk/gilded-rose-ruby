7b92094546701a17c482a1c8c2761ebe48358036
Michael Kingma <mikingma@memphis.edu>
2024-02-28 20:34:25 -0600

    Wrote brie_tick method all tests pass

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -60,7 +60,11 @@ class GildedRose
     end
   end
   def brie_tick
+    @days_remaining -= 1
+    return if @quality >= 50
 
+    @quality += 1
+    @quality += 1 if @days_remaining <= 0 && @quality < 50
   end
   def normal_tick
     @days_remaining -= 1
